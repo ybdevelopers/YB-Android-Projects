76dc770ce0cee6dbb6335d09ca4d90a526e4cb44
Yash Bhatt <70092000+ybdevelopers@users.noreply.github.com>
2021-06-08 23:29:49 +0530

    - Completed Design of listing and added navigation controller setup in project.

--- MobileFeature/app/src/main/java/com/ybdevelopers/mobilefeature/MainActivity.kt
+++ MobileFeature/app/src/main/java/com/ybdevelopers/mobilefeature/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.ybdevelopers.mobilefeature
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.ybdevelopers.mobilefeature.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_container)
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
